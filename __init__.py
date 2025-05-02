8e4eb9a043c0e40942e868f1b70ae5814e5640a1
SdRfredyan <83364412+SdRfredyan@users.noreply.github.com>
2025-05-02 15:16:04 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,8 +16,9 @@ def est_utilisateur_authentifie():
     return session.get('utilisateur_authentifie')
 
 @app.route('/')
-def hello_world():
-    return render_template('hello.html')
+def accueil():
+    marques = Marque.query.all()
+    return render_template('hello.html', marques=marques)
 
 @app.route('/lecture')
 def lecture():
