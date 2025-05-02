67f07c04e1354c2a16f89397d4dbb9850e85801c
SdRfredyan <83364412+SdRfredyan@users.noreply.github.com>
2025-05-02 16:49:05 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from flask import json
 from urllib.request import urlopen
 from werkzeug.utils import secure_filename
 import sqlite3
+from models import db, Marque
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les session
