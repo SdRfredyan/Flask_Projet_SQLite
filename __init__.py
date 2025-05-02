469706c1b49aacbb8e9b51792f8d41f431343ae6
SdRfredyan <83364412+SdRfredyan@users.noreply.github.com>
2025-05-02 15:46:28 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from models import db, Marque
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'votre-cle-secrete'
 
