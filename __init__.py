e2376e9b21a9764b231b7c39d6fba7333845fcc2
SdRfredyan <83364412+SdRfredyan@users.noreply.github.com>
2025-05-02 16:18:12 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,95 @@
-from flask import Flask, render_template
-import models
+from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
+from flask import render_template
+from flask import json
+from urllib.request import urlopen
+from werkzeug.utils import secure_filename
+import sqlite3
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'ma-cle-secrete'
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les session
 
-models.db.init_app(app)
+# Fonction pour créer une clé "authentifie" dans la session utilisateur
+def est_authentifie():
+    return session.get('authentifie')
+
+def est_utilisateur_authentifie():
+    return session.get('utilisateur_authentifie')
 
 @app.route('/')
 def accueil():
-    with app.app_context():
-        marques = models.Marque.query.all()
+    marques = Marque.query.all()
     return render_template('hello.html', marques=marques)
+
+@app.route('/lecture')
+def lecture():
+    if not est_authentifie():
+        return redirect(url_for('authentification'))
+    return "<h2>Bravo, vous êtes authentifié</h2>"
+
+@app.route('/authentification', methods=['GET', 'POST'])
+def authentification():
+    if request.method == 'POST':
+        if request.form['username'] == 'admin' and request.form['password'] == 'password':
+            session['authentifie'] = True
+            return redirect(url_for('lecture'))
+        elif request.form['username'] == 'user' and request.form['password'] == '12345':
+            session['utilisateur_authentifie'] = True
+            return redirect(url_for('hello_world'))
+        else:
+            return render_template('formulaire_authentification.html', error=True)
+    
+    return render_template('formulaire_authentification.html', error=False)
+
+@app.route('/fiche_client/<int:post_id>')
+def Readfiche(post_id):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('read_data.html', data=data)
+
+@app.route('/fiche_nom/<nom>')
+def search_by_name(nom):
+    if not est_utilisateur_authentifie():
+        return redirect(url_for('authentification'))
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE UPPER(nom) = UPPER(?)', (nom,))
+    data = cursor.fetchall()
+    conn.close()
+    if data:
+        return render_template('read_data.html', data=data)
+    else:
+        return "<h2>Aucun client trouvé avec ce nom.</h2>"
+
+@app.route('/consultation/')
+def ReadBDD():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients;')
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('read_data.html', data=data)
+
+@app.route('/enregistrer_client', methods=['GET'])
+def formulaire_client():
+    return render_template('formulaire.html')
+
+@app.route('/enregistrer_client', methods=['POST'])
+def enregistrer_client():
+    nom = request.form['nom']
+    prenom = request.form['prenom']
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO clients (created, nom, prenom, adresse) VALUES (?, ?, ?, ?)', (1002938, nom, prenom, "ICI"))
+    conn.commit()
+    conn.close()
+    return redirect('/consultation/')
+
+
+                                                                                                                                       
+if __name__ == "__main__":
+  app.run(debug=True)
