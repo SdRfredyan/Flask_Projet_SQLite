d040c43aa2cebf8ed4bca2bfa285e97974f70888
SdRfredyan <83364412+SdRfredyan@users.noreply.github.com>
2025-05-05 08:56:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template_string, render_template, jsonify, reque
 from flask import render_template
 from flask import json
 from urllib.request import urlopen
-from models import db, Marque
 from werkzeug.utils import secure_filename
 import sqlite3
 
