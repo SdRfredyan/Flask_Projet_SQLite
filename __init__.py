d278c896bdb5eb55ee459edc17983f4292ee0fbd
SdRfredyan <83364412+SdRfredyan@users.noreply.github.com>
2025-05-07 11:25:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ import sqlite3
 
 
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les session
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessionm
 
 # Fonction pour créer une clé "authentifie" dans la session utilisateur
 def est_authentifie():
