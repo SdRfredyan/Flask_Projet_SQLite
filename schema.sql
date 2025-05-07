b6855fa9f0c0af7b9c38b21fb737d0348fa838c4
SdRfredyan <83364412+SdRfredyan@users.noreply.github.com>
2025-05-07 13:55:26 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS marque;
 DROP TABLE IF EXISTS modele;
+DROP TABLE IF EXISTS moteur;
+DROP TABLE IF EXISTS finition;
 
 CREATE TABLE marque (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -10,10 +12,23 @@ CREATE TABLE modele (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nom TEXT NOT NULL,
     annee TEXT,
-    motorisation TEXT,
-    consommation TEXT,
-    fiabilite TEXT,
-    description TEXT,
-    marque_id INTEGER NOT NULL,
+    image TEXT,
+    marque_id INTEGER,
     FOREIGN KEY (marque_id) REFERENCES marque(id)
+);
+
+CREATE TABLE moteur (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    modele_id INTEGER,
+    type TEXT CHECK(type IN ('essence', 'diesel')),
+    nom TEXT,
+    fiabilite TEXT CHECK(fiabilite IN ('fiable', 'moyenne', 'non')),
+    FOREIGN KEY (modele_id) REFERENCES modele(id)
+);
+
+CREATE TABLE finition (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    modele_id INTEGER,
+    nom TEXT,
+    FOREIGN KEY (modele_id) REFERENCES modele(id)
 );
