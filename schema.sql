2358503c9cfd07e967fbdda63e0626552ef1e1ed
SdRfredyan <83364412+SdRfredyan@users.noreply.github.com>
2025-05-07 14:12:15 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,34 +1,36 @@
 DROP TABLE IF EXISTS marque;
 DROP TABLE IF EXISTS modele;
-DROP TABLE IF EXISTS moteur;
+DROP TABLE IF EXISTS motorisation;
 DROP TABLE IF EXISTS finition;
 
 CREATE TABLE marque (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nom TEXT NOT NULL
+    nom TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE modele (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nom TEXT NOT NULL,
-    annee TEXT,
-    image TEXT,
-    marque_id INTEGER,
+    annee_debut INTEGER,
+    annee_fin INTEGER,
+    description TEXT,
+    marque_id INTEGER NOT NULL,
     FOREIGN KEY (marque_id) REFERENCES marque(id)
 );
 
-CREATE TABLE moteur (
+CREATE TABLE motorisation (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    modele_id INTEGER,
-    type TEXT CHECK(type IN ('essence', 'diesel')),
-    nom TEXT,
-    fiabilite TEXT CHECK(fiabilite IN ('fiable', 'moyenne', 'non')),
+    modele_id INTEGER NOT NULL,
+    type TEXT CHECK(type IN ('essence', 'diesel')) NOT NULL,
+    nom TEXT NOT NULL, -- Exemple : '2.0L S16'
+    puissance TEXT,    -- Exemple : '100 - 150 cv'
+    fiabilite TEXT CHECK(fiabilite IN ('fiable', 'moyenne', 'non')) NOT NULL,
     FOREIGN KEY (modele_id) REFERENCES modele(id)
 );
 
 CREATE TABLE finition (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    modele_id INTEGER,
-    nom TEXT,
+    modele_id INTEGER NOT NULL,
+    nom TEXT NOT NULL,
     FOREIGN KEY (modele_id) REFERENCES modele(id)
 );
